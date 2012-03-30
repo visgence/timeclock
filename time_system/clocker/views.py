6b96181fc296826a0435408d48d993ac9be77d24
Bretton Murphy <bmurphy@visgence.com>
2012-03-29 23:28:20 -0600

    template now outputting total hours. Still need to round to nearest 15 minutes but besides that all functionality is done

--- time_system/clocker/views.py
+++ time_system/clocker/views.py
@@ -1,13 +1,50 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from models import Employee
+from models import Employee, Time
+from datetime import timedelta, datetime, time
 from check_access import check_access
 
 
 def total_hours(request):
-    return render_to_response('total_hours.html', context_instance=RequestContext(request))
+
+    if(request.method == 'POST'):
+        user_name = request.POST.get('user_name')
+        start_time = request.POST.get('from')
+        end_time = request.POST.get('to')
+        time_info = {'employee':user_name, 'times':[]}
+
+        start_time = datetime.strptime(start_time, '%Y-%m-%d')
+        end_time = datetime.strptime(end_time, '%Y-%m-%d')
+        end_time += timedelta(1)
+        num_days = end_time - start_time
+
+        time = Time.objects.filter(employee__user__username = user_name).filter(time_in__gte = start_time.date()).filter(time_in__lt = end_time.date())
+
+        if(len(time) > 0):
+            total_time = datetime.strptime("00:00:00", '%H:%M:%S')
+            #total_time = datetime.time()
+
+            for day in range(num_days.days):
+                date = time.filter(time_in__day = start_time.day)
+                sum_time = datetime.strptime("00:00:00", '%H:%M:%S')
+
+                for record in date:
+                    print record
+                    sum_time += record.time_out - record.time_in
+                    total_time += record.time_out - record.time_in
+                    #print sum_time
+                
+                #print datetime.strftime(total_time, "%H:%M")
+
+                #time_info['times'][datetime.strftime(start_time, "%Y-%m-%d %H:%M"), datetime.strftime(sum_time, "%H:%M")]
+                time_info['times'].append([datetime.strftime(start_time, "%Y-%m-%d %H:%M"), datetime.strftime(sum_time, "%H:%M")])
+                start_time += timedelta(1)
+            time_info['total'] = datetime.strftime(total_time, "%H:%M")
+            print time_info
+
+    return render_to_response('total_hours.html', {'employee_hours':time_info}, context_instance=RequestContext(request))
 
 def main_page(request):
     """
