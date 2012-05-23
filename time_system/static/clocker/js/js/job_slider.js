b1e86dcd77da141ef459776db456fd958271cb14
David Salas <david@visgence.com>
2012-05-22 18:33:58 -0600

    template

--- time_system/static/clocker/js/js/job_slider.js
+++ time_system/static/clocker/js/js/job_slider.js
@@ -4,24 +4,29 @@
  *script: This is script's primary functions is to find all jquery ui sliders on the page and
  *to ensure that they add up to the total amount worked for that day.
  * */
-$("document").ready(function ()
+
+
+function create_slider_handler(job_id)
 {
-    var d = new Date();
-
-    //Find all job slider divs
-    divs = $(".job_slider");
-
-    //bind each one
-    for (var i = 0; i < divs.length; i++) 
-    {   
-        var datastream_id = divs[i].id;
-    }
-
-
-
-    $("#job1").slider
+    return $("#" + job_id).slider
     ({
         slide: function(event, ui) {  }
     });
+
+}//end create_slider_handler 
+
+
+
+$("document").ready(function ()
+{
+
+    //Find all job slider divs
+    job_ids = $(".job_slider");
+
+    //create a handler for each slider
+    for (var i = 0; i < job_ids.length; i++) 
+    {   
+        create_slider_handler(job_ids[i].id)
+    }
 
 })
