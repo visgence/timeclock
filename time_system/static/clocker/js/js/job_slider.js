1a284c087c4b60088cab7a40f0d2a381eb978845
David Salas <david@visgence.com>
2012-05-23 15:48:17 -0600

    working on job_slider.js

--- time_system/static/clocker/js/js/job_slider.js
+++ time_system/static/clocker/js/js/job_slider.js
@@ -6,24 +6,37 @@
  * */
 
 
-function create_slider_handler(job_id)
+function create_slider_handler(job_id, total_time)
 {
+
+    //alert(total_time);
     return $("#" + job_id).slider
     ({
         slide: function(event, ui)
         {
-            alert(ui);
-            var hours = Math.floor(ui.value / 3600);
-            var minutes = Math.floor(ui.value - (hours * 3600))/60);
-            //var minutes = Math.floor((ui.value - (hours * 3660))/60);
+            var hours = Math.floor(ui.value / 60);
+            var minutes = Math.floor(ui.value - (hours * 3600)/60);
 
-            if(hours.length == 1) hours = '0' + hours;
-            if(minutes.length == 1) minutes = '0' + minutes;
+            //Handle the case where minustes > 60
+            if(minutes%60 == 0 && minutes != 0) 
+            {
+                hours =  hours + 1;
+                //alert("hours: " + hours + " minutes: " + minutes);
+                minutes = minutes - (hours * 60)
+            }
+
+            if(hours < 10) hours = '0' + hours;
+            if(minutes < 10) minutes = '0' + minutes;
 
             $('#hours_' + job_id).val(hours+':'+minutes); 
 
+
+            var time = time_to_sec($("#total_time").val());
+            $('#debug').html('total_seconds: ' + time);
+            $("#total_time").val(sec_to_time(total_time - (minutes*60) - (hours*3600)));
+
         },
-        max:$("#total_time").val()
+        max:Math.floor(total_time/60)
 
     });
 
@@ -34,16 +47,17 @@ function create_slider_handler(job_id)
 $("document").ready(function ()
 {
     //Convert total time from seconds to hour:min
-    total_time = sec_to_time($("#total_time").val();
-    $("#total_time").val(total_time);
+    total_time = $("#total_time").val();
+    $("#total_time").val(sec_to_time(total_time));
 
+    time_to_sec($("#total_time").val());//DEBUG
     //Find all job slider divs
     job_ids = $(".job_slider");
 
     //create a handler for each slider
     for (var i = 0; i < job_ids.length; i++) 
     {   
-        create_slider_handler(job_ids[i].id);
+        create_slider_handler(job_ids[i].id, total_time);
     }
 
 })
@@ -51,10 +65,59 @@ $("document").ready(function ()
 function sec_to_time(sec)
 {
     var hours = Math.floor(sec / 3600);
-    var minutes = Math.floor((sec - (hours * 3660))/60);
+    var minutes = Math.floor((sec - (hours * 3600))/60);
 
-    if(hours.length == 1) hours = '0' + hours;
-    if(minutes.length == 1) minutes = '0' + minutes;
+    if(hours < 10) hours = '0' + hours;
+    if(minutes < 10) minutes = '0' + minutes;
     
     return hours + ':' + minutes;
+}//end sec_to_time
+
+//Parses a time string in the form 'hh:mm' and converts to seconds
+//Returns: Integer seconds
+function time_to_sec(time)
+{
+    //pull the hours out
+    var hours = time.substr(0,2);
+    
+    //alert(hours);
+    if(hours != '00')
+    {
+        //if there is a leading zero, chuck it.
+        if(hours.substr(0,1) == '0')
+        {
+            hours = parseInt(hours.substr(1,1));
+            //alert(hours);
+        }
+        else//both digits are significant
+        {
+            hours = parseInt(minutes.substr(0,2));
+            //alert(hours);
+        }
+
+
+    }
+    else{hours = 0;}
+
+    //pull out the minutes
+    var minutes= time.substr(3,2);
+    if(minutes != '00')
+    {
+        //if there is a leading zero, chuck it.
+        if(minutes.substr(0,1) == '0')
+        {
+            minutes = parseInt(minutes.substr(1,1));
+            //alert(minutes);
+        }
+        else//both digits are significant
+        {
+            minutes = parseInt(minutes.substr(0,2));
+            //alert(minutes);
+        }
+
+    }
+    else{minutes = 0;}
+    
+    //$('#debug').html('total_seconds: ' + Math.floor((hours * 3600) + ( minutes * 60)))
+    return  Math.floor((hours * 3600) + ( minutes * 60));
 }//end sec_to_time
