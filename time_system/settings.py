9486b4df0e39dd90b1f386c20ea5688e95697fcc
Bretton Murphy <bmurphy@visgence.com>
2013-04-26 12:39:54 -0600

    Fix bug in settings

--- time_system/settings.py
+++ time_system/settings.py
@@ -71,7 +71,6 @@ STATIC_URL = '/static/'
 #ADMIN_MEDIA_PREFIX = '/static/admin/' 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    APP_PATH + 'static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
