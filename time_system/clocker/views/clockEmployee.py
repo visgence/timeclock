9442eb3292152ed8b33ff11a84e7f998fb46d7e7
djsubaku@gmail.com <bmurphy@visgence.com>
2013-07-24 10:54:01 -0600

    Make seperate view for clocking employee in/out

--- time_system/clocker/views/clockEmployee.py
+++ time_system/clocker/views/clockEmployee.py
@@ -1,28 +1,60 @@
 from django.views.decorators.http import require_POST
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from clocker.models import Employee
 
 
 @require_POST
 def clockEmployee(request):
-    
+    '''
+    ' View that clocks an employee in or out depending on the value of "status"
+    ' in the POST request.
+    '
+    ' Returns HttpResponseRedirect to the next view
+    '''
+
     status = request.POST.get('status', None)
     if status is None:
         return HttpResponseNotFound()
 
     employee = request.user
-    if status.lower() == "out" and not employee.isClockedIn():
-        return clockOutEmployee(employee)
-    elif status.lower() == "in" and employee.isClockedIn():
-        return clockInEmployee(employee)
+    if status.lower() == "out" and employee.isClockedIn():
+        clockOutEmployee(employee)
+        return HttpResponseRedirect('/timeclock/')
+    elif status.lower() == "in" and not employee.isClockedIn():
+        clockInEmployee(employee)
+        return HttpResponseRedirect('/timeclock/')
 
-    return "something nasty"
+    return HttpResponseNotFound()
 
 
 def clockOutEmployee(employee):
+    '''
+    ' Clocks an employee out.
+    '
+    ' Returns: Shift used to clock employee out with.
+    '''
     
-    pass
+    if not isinstance(employee, Employee):
+        return HttpResponseNotFound()
+
+    try:
+        return employee.clock_out()
+    except Exception as e:
+        return HttpResponseNotFound(str(e))
 
 
 def clockInEmployee(employee):
+    '''
+    ' Clocks an employee in.
+    '
+    ' Returns: Shift created to clock employee in with.
+    '''
 
-    employee.clock_in()
+    if not isinstance(employee, Employee):
+        return HttpResponseNotFound()
+    
+    try:
+        return employee.clock_in()
+    except Exception as e:
+        return HttpResponseNotFound(str(e))
+
