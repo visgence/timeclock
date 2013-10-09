4011c6772bd134ca92308115ad411816485af659
djsubaku@gmail.com <bmurphy@visgence.com>
2013-10-09 15:40:29 -0600

    Make change password use bootstraps modal dialog

--- time_system/clocker/static/js/utils.js
+++ time_system/clocker/static/js/utils.js
@@ -1,126 +1,92 @@
 
-/** This method will pop up a dialog that will allow the user to change their password.
-  *
-  */
-function changePassDialog()
-{
-    var ok = {
-        text: 'OK',
-        click: function() {
-            $(this).dialog('close');
-        }
-    };
+function initPassDialog() {
+    $('#change-pass-modal').modal({
+        'show': false
+        ,'backdrop': 'static'
+    })
+    .on('hidden.bs.modal show.bs.modal', function() {
+        resetPassForm();   
+    }); 
+        
+    $('#change-pass-link').on('click', function() {
+        $('#change-pass-modal').modal('show');
 
-    var buttons = [
-        {
-            text: 'Change Password',
-            click: function() {
-                // Send the change password request
-                var postData = {};
-                var self = this;
-
-                postData.oldPassword = $('#oldPassword').val();
-                postData.newPassword = $('#passwordInput').val();
-
-                if ( postData.oldPassword === '' ) {
-                    makeDialog('Please enter your old password', 'Error', [ok]);
-                    return;
-                }
-                if ( postData.newPassword === '' ) {
-                    makeDialog('Please enter your new password', 'Error', [ok]);
-                    return;
-                }
-                if ( postData.newPassword !== $('#passwordReinput').val() ) {
-                    makeDialog('Your new passwords do not match.  Please reenter them.', 'Error', [ok]);
-                    return;
-                }
-                var csrf = $('input[name="csrfmiddlewaretoken"]').val();
-                $.post('/changePassword/', {csrfmiddlewaretoken: csrf,
-                                                      jsonData: JSON.stringify(postData)},
-                       function(resp) {
-                           if(resp.accessError)
-                               makeDialog(resp.accessError, 'Error', [{text:'OK', click: function(){window.location = "/"}}]);
-                           else if ( resp.errors )
-                               makeDialog(resp.errors, 'Error', [ok]);
-                           else if ( resp.success ) {
-                               makeDialog(resp.success, 'Success', [ok]);
-                               $(self).dialog('close');
-                           }
-                           else
-                               makeDialog(resp, 'Unknown Error', [ok]);
-                       });
-            }
-        },
-        {
-            text: 'Cancel',
-            click: function() {
-                $(this).dialog('close');
-            }
-        }
-    ];
+        //Return false since it's a link we're clicking on
+        return false;
+    });
     
-    $.get('/passwordForm/', {}, function(resp) {
-        if(resp.accessError)
-            makeDialog(resp.accessError, 'Error', [{text:'OK', click: function(){window.location = "/"}}]);
-        else if ( resp.errors )
-            makeDialog(resp.errors, 'Error', [{text:'OK', click: function(){$(this).dialog('close');}}]);
-        else if ( resp.html )
-            makeDialog(resp.html, 'Change Password', buttons);
-        else
-            makeDialog(resp, 'Unknown Error', [{text:'OK', click: function(){$(this).dialog('close');}}]);
+    $('#change-pass-btn').on('click', function() {
+        changePassword();
     });
 }
 
+function passError(error) {
+    $('#pass-msg').addClass('alert alert-danger').html(error);
+}
 
-/** This method will create a dialogue and insert content from an ajax call
- *  into it.
- *
- * \param[in] msg   The message (html) to use in the dialog.
- * \param[in] title  The title on the dialog.
- * \param[in] buttons An array of js objects.  Must follow the jquery-ui dialog button specification:
- *                    E.g.  buttons = [{text: 'Ok', click: function(){ $(this).dialog('close'); } }, ...]
- */
-function makeDialog(msg, title, buttons)
-{
-    var div = $('<div></div>');
+function resetPassError() {
+    $('#pass-msg').removeClass('alert alert-danger').html('');
+}
+
+function resetPassForm() {
+    resetPassError()
+    $('#change-pass-modal form').get(0).reset();
+}
+
+/** This method will pop up a dialog that will allow the user to change their password.
+  *
+  */
+function changePassword() {
+    var csrf = $('input[name="csrfmiddlewaretoken"]').val();
+    var jsonData = {
+        oldPassword: $('#oldPassword').val()
+        ,newPassword: $('#passwordInput').val()
+    }
+
+    if ( jsonData.oldPassword.trim() === '' ) {
+        passError('Please enter your old password');
+        return;
+    }
+    if ( jsonData.newPassword.trim() === '' ) {
+        passError('Please enter your new password');
+        return;
+    }
+    if ( jsonData.newPassword !== $('#passwordReinput').val().trim() ) {
+        passError('Your new passwords do not match.');
+        return;
+    }
+
+    var self = this;
+    var postData = {
+        csrfmiddlewaretoken: csrf
+        ,jsonData: JSON.stringify(jsonData)
+    };
     
-    $(div).addClass('serverDialog');
-
-    // Find all the serverDialogs, and find the one with the largest id
-    var IDs = $.map($('.serverDialog'), function(e, i) {
-        console.log(e);
-        console.log($(e).data('id'));
-        return $(e).data('id');
+    $.post('/changePassword/', postData, function(resp) {
+        if(resp.accessError)
+            window.location = '/';
+        else if ( resp.errors )
+            passError(resp.errors);
+        else if ( resp.success ) {
+            $('#change-pass-modal').modal('hide');
+            sysDialog(resp.success, "Success!", "success");
+        }
+        else
+            passError("An unknown error has occured");
     });
+}
 
-    var data_id = 0;
+function sysDialog(msg, title, type) {
+    var types = {
+        'error': 'has-error'
+        ,'success': 'has-success'
+        ,'warning': 'has-warning'
+    };
 
-    if ( IDs.length !== 0 )
-        data_id = Math.max.apply(null,IDs) + 1; 
+    if(types.hasOwnProperty(type))
+        $('#sys-modal div.modal-body').addClass(types[type]);
 
-    var id = 'serverDialog' + data_id;
-
-    $(div).data('id', data_id);
-    $(div).attr('id', id);
-    $(div).css('display', 'none');
-    $(div).attr('title', title);
-    $(div).html(msg);
-    $('body').append(div);
-    $('#' + id).dialog({
-        autoOpen: true,
-        resizable: true,
-        hide: 'fade',
-        show: 'fade',
-        modal: true,
-        minWidth: 475,
-        maxWidth: 1000,
-        minHeight: 300,
-        maxHeight: 1000,
-        dialogClass: "dialogue",
-        close: function() {
-            $(this).dialog('destroy');
-            $('#' + id).remove();
-        },
-        buttons: buttons
-    });
+    $('#sys-modal div.modal-body').children('span').html(msg);
+    $('#sys-modal .modal-title').html(title);
+    $('#sys-modal').modal('show');
 }
