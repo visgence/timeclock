94f8db6fc1471645f63372e3e951cb6cedbe418c
djsubaku@gmail.com <bmurphy@visgence.com>
2014-01-15 12:38:20 -0700

    Make setup command optionaly load new job fixtures

--- time_system/clocker/management/commands/setup.py
+++ time_system/clocker/management/commands/setup.py
@@ -42,7 +42,11 @@ class Command(BaseCommand):
             user_resp = raw_input('(y/n) ')
 
         if user_resp in ['y', 'Y']:
-            fixtures.append([])
+            fixtures.append([
+                [
+                    "clocker/fixtures/jobs.json"
+                ]
+            ])
 
         # Load fixtures
         for apps in fixtures:
