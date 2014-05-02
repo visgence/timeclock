9789603841a3133c94554acf1e8311b3aae14934
djsubaku@gmail.com <bmurphy@visgence.com>
2014-05-02 17:29:13 -0600

    Change math_helper prec back to 4

--- time_system/clocker/templatetags/math_helper.py
+++ time_system/clocker/templatetags/math_helper.py
@@ -2,7 +2,7 @@ from django import template
 from decimal import *
 register = template.Library()
 
-myothercontext = Context(prec=2)
+myothercontext = Context(prec=4)
 @register.filter
 def to_hour(value):
     setcontext(myothercontext)
