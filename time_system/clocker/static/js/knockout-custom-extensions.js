29db443bad24025e4704b7f96ca8554ef89b5669
djsubaku@gmail.com <bmurphy@visgence.com>
2014-06-02 13:57:18 -0600

    Added required custom knockout extension to project

---
+++ time_system/clocker/static/js/knockout-custom-extensions.js
@@ -0,0 +1,31 @@
+$(function() {
+    "use strict";
+    
+    ko.extenders.required = function(target, overrideMessage) {
+        if (!target.hasOwnProperty('hasError'))
+            target.hasError = ko.observable();
+        if (!target.hasOwnProperty('validationMessage'))
+            target.validationMessage = ko.observable();
+        
+        target.validate = function() {
+            var value = target();
+            if(typeof(value) === "string")
+                value = value.trim();
+
+            var error = false;
+            if($.isArray(value))
+                error = value.length > 0 ? false : true;
+            else
+                error = value ? false : true;
+
+            target.hasError(error);
+            target.validationMessage(error ? overrideMessage || "" : "");
+
+            //Return inverse to say whether or not validation succeeded or failed for target
+            return !target.hasError();
+        };
+
+        return target;
+    };
+    
+});
