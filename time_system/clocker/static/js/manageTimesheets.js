e09380bc02893cc1038ebe5fceeb48e375e96f39
djsubaku@gmail.com <bmurphy@visgence.com>
2014-06-18 15:49:53 -0600

    Manage toggled timesheets in url

--- time_system/clocker/static/js/manageTimesheets.js
+++ time_system/clocker/static/js/manageTimesheets.js
@@ -45,11 +45,32 @@ $(function() {
             return true;
         }.bind(this);
 
+        //Called when user clicks on a timesheet to toggle open/close
+        var toggleTsCallback = function(e) {
+            var state = $.bbq.getState();
+            var ts = $(e.target).data("target");
+
+            //If timesheet is already the current state just toggle it
+            if (state.hasOwnProperty("timesheet") && state.timesheet == ts) {
+                var collapsable = $("#timesheet-"+state.timesheet);
+                var targetTs = ko.dataFor(collapsable.get(0));
+                if (!targetTs.isBusy())
+                    $(collapsable).collapse('toggle');
+            }
+            else
+                $.bbq.pushState({"timesheet": ts});
+        };
+
+        //Called when the timesheets list finishes refreshing during module init.
+        var tsRefreshCallback = function(resp) {
+            prepareCollapses();
+            $(window).trigger("hashchange")
+        };
+
+        //Add needed hooks to bootstrap collapse elements for when a user tries to toggle a timesheet.
         var prepareCollapses = function() {
-            $("#timesheet-accordion").on("show.bs.collapse", function(e) {
-                var targetTs = ko.dataFor(e.target);
-                targetTs.loadPayData();
-            })
+            $('.panel-collapse').collapse({toggle: false, parent: "#timesheet-accordion"});
+            $("#timesheet-accordion").on("click", "a.collapse-toggle", toggleTsCallback);
         };
 
         var init = function(vars) {
@@ -60,12 +81,29 @@ $(function() {
                 this.employeeOptions(vars.employeeOptions);            
             }
 
-            timesheets.refresh().done(prepareCollapses);
+            timesheets.refresh().done(tsRefreshCallback);
+
             setupPickers();
 
             var newTs = {"messageCenter": this.messageCenter()};
             this.newTimesheet(new Timesheet(newTs));
+            $(window).on('hashchange', hashchange);
         }.bind(this);
+
+        var hashchange = function() {
+            var state = $.bbq.getState();
+
+            if (state.hasOwnProperty('timesheet')) {
+                var collapsable = $("#timesheet-"+state.timesheet);
+                var targetTs = ko.dataFor(collapsable.get(0));
+
+                //Wait till data is loaded before opening collapse
+                targetTs.loadPayData().then(function() {
+                    $(collapsable).collapse('show');
+                });
+            }
+
+        };
 
         init(vars);
     };
