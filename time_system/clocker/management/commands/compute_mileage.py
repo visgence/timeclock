08528bc35c9f4a5e5798f091c5c82a7cf2be1f0d
Michael Harris <mharris@visgence.com>
2015-08-14 14:27:51 -0600

    adds compute_mileage

---
+++ time_system/clocker/management/commands/compute_mileage.py
@@ -0,0 +1,34 @@
+# compute mileage, this stuff is just the output of an iPython session and needs to be made into a proper command
+
+s = Shift.object.all()
+s = Shift.objects.all()
+s = Shift.objects.all()[0]
+s.time_out
+s.time_in
+s.time_out
+s = Shift.objects.all()[-10]
+s = Shift.objects.all()[10]
+s.time_out
+import datetime
+datetime.datetime(2015, 7, 16, 0, 0, 0, 0)
+start = datetime.datetime(2015, 7, 16, 0, 0, 0, 0)
+end = datetime.datetime(2015, 7, 31, 23, 59, 59, 0)
+end
+ShiftSummary.objects.filter(shift__time_out__gte=start)
+summaries = ShiftSummary.objects.filter(shift__time_out__gte=start)
+summaries.filter(shift__time_out__lte=end)
+summaries = summaries.filter(shift__time_out__lte=end)
+summaries[0].shift
+summaries[0].shift.employee
+summaries[0].miles
+for i in summaries:
+    print i.miles
+
+total_miles = 0
+for i in summaries:
+    total_miles+=i.miles
+
+total_miles
+get_ipython().magic(u'save mileage_calc')
+get_ipython().magic(u'save mileage_calc 0-27')
+
