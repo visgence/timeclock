6ddbbca9adc6ccb6f5e2bde1c4f2ba0653b800c3
Evan Salazar <evan@visgence.com>
2015-12-10 09:39:24 -0700

    Adding find missing discription script

---
+++ time_system/find_missing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os, re, sys
+import time
+from datetime import datetime
+program_path = os.path.realpath(__file__)
+APP_PATH = re.sub('settings.py[c]*$','',program_path);
+sys.path.append(os.path.join(APP_PATH,'time_system'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from django.conf import settings
+#local imports 
+from clocker.models import Shift,ShiftSummary
+from clocker.views.job import getWeekdayRange
+month = 3600*24*31
+start = datetime.fromtimestamp(time.time()-month)
+shifts = Shift.objects.filter(time_in__gte=start).exclude(time_out=None)
+for s in shifts:
+    if len(ShiftSummary.objects.filter(shift=s)) == 0:
+        print s
+
