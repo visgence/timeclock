98f8e8fd0a6e3627ada2f130d090d6328f09e52d
Cyrille Gindreau <cyrille@visgence.com>
2017-08-30 15:38:16 -0600

    made script into a function that returns array of missing scripts

--- time_system/find_missing.py
+++ time_system/find_missing.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
-import os, re, sys
+import os
+import re
+import sys
 import time
 from datetime import datetime
-program_path = os.path.realpath(__file__)
-APP_PATH = re.sub('settings.py[c]*$','',program_path);
-sys.path.append(os.path.join(APP_PATH,'time_system'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 import django
 from django.conf import settings
-django.setup()
-#local imports 
-from clocker.models import Shift,ShiftSummary
-from clocker.views.job import getWeekdayRange
-month = 3600*24*31
-start = datetime.fromtimestamp(time.time()-month)
-shifts = Shift.objects.filter(time_in__gte=start).exclude(time_out=None)
-for s in shifts:
-    if len(ShiftSummary.objects.filter(shift=s)) == 0:
-        print s
 
+
+program_path = os.path.realpath(__file__)
+APP_PATH = re.sub('settings.py[c]*$', '', program_path)
+sys.path.append(os.path.join(APP_PATH, 'time_system'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+django.setup()
+
+# local imports
+from clocker.models import Shift, ShiftSummary
+from clocker.views.job import getWeekdayRange
+
+
+def findMissing():
+    month = 3600*24*31
+    start = datetime.fromtimestamp(time.time()-month)
+    shifts = Shift.objects.filter(time_in__gte=start).exclude(time_out=None)
+    shiftsArr = []
+    for s in shifts:
+        if len(ShiftSummary.objects.filter(shift=s)) == 0:
+            print s
+            shiftsArr.append(s)
+    return shiftsArr
+
+
+if __name__ == "__main__":
+    findMissing()
