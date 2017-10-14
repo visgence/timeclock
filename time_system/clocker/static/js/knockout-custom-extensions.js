b9c5aab2397b319d831cfc764e0ce0c7bb1674a0
Cyrille Gindreau <cyrille@visgence.com>
2017-10-14 10:59:29 -0600

    removing bootstrap-timepicker and lint

--- time_system/clocker/static/js/knockout-custom-extensions.js
+++ time_system/clocker/static/js/knockout-custom-extensions.js
@@ -1,31 +1,31 @@
-$(function() {
-    "use strict";
-    
-    ko.extenders.required = function(target, overrideMessage) {
-        if (!target.hasOwnProperty('hasError'))
+$(() => {
+    ko.extenders.required = function (target, overrideMessage) {
+        if (!target.hasOwnProperty('hasError')) {
             target.hasError = ko.observable();
-        if (!target.hasOwnProperty('validationMessage'))
+        }
+        if (!target.hasOwnProperty('validationMessage')) {
             target.validationMessage = ko.observable();
-        
-        target.validate = function() {
-            var value = target();
-            if(typeof(value) === "string")
+        }
+
+        target.validate = function () {
+            let value = target();
+            if (typeof(value) === 'string') {
                 value = value.trim();
+            }
 
-            var error = false;
-            if($.isArray(value))
+            let error = false;
+            if ($.isArray(value)) {
                 error = value.length > 0 ? false : true;
-            else
+            } else {
                 error = value ? false : true;
-
+            }
             target.hasError(error);
-            target.validationMessage(error ? overrideMessage || "" : "");
+            target.validationMessage(error ? overrideMessage || '' : '');
 
-            //Return inverse to say whether or not validation succeeded or failed for target
+            // Return inverse to say whether or not validation succeeded or failed for target
             return !target.hasError();
         };
 
         return target;
     };
-    
 });
